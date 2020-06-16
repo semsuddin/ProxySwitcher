48b3e0b3868e8961a2eea00fe0da91a93090b9e5
Semsudin Sefic <semsudin.sefic@alfalaval.com>
2020-06-16 10:21:16 +0200

    Add exterminate feature

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -98,6 +99,12 @@ namespace ProxySwitcher
                 string valueOfSelection = newItemSelected.Name.ToLower();
                 Environment.SetEnvironmentVariable(ENVIRONMENT_SELECTOR_KEY, valueOfSelection, EnvironmentVariableTarget.User);
             }
+        }
+
+        private void Button_Exterminate(object sender, RoutedEventArgs e)
+        {
+            var taskToKill = TaskToKill.Text;
+            System.Diagnostics.Process.Start("taskkill", "/F /IM " + taskToKill);
         }
     }
     public static class ProxyHandler
